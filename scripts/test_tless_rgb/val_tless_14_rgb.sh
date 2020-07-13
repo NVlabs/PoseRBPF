c6da8360f39709d39bed1f0f89e0c2423b061577
Xinke Deng <xdeng12@illinois.edu>
2020-07-12 21:12:15 -0500

    tless rgb scripts

---
+++ scripts/test_tless_rgb/val_tless_14_rgb.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES=$1 python test_prbpf_rgb.py --test_config './config/test/test_single_obj_list_tless/14.yml' \
+                                                  --pf_config_dir './config/test/TLess/' \
+                                                  --train_config_dir './checkpoints/tless_configs_rgb/' \
+                                                  --ckpt_dir './checkpoints/tless_ckpts_rgb/' \
+                                                  --codebook_dir './checkpoints/tless_codebooks_rgb/' \
+                                                  --dataset 'tless' \
+                                                  --dataset_dir '../TLess/t-less_v2/test_primesense/' \
+                                                  --n_seq $2;
---
+++ scripts/test_tless_rgbd/val_tless_23_rgbd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES=$1 python test_prbpf_rgbd.py --test_config './config/test/test_single_obj_list_tless/23.yml' \
+                                                  --pf_config_dir './config/test/TLess/' \
+                                                  --train_config_dir './checkpoints/tless_configs_roi_rgbd/' \
+                                                  --ckpt_dir './checkpoints/tless_ckpts_roi_rgbd/' \
+                                                  --codebook_dir './checkpoints/tless_codebooks_roi_rgbd/' \
+                                                  --dataset 'tless' \
+                                                  --dataset_dir '../TLess/t-less_v2/test_primesense/' \
+                                                  --n_seq $2;
