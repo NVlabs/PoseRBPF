f371c4197964e302fd52e46caa5afceea6ecfcea
yuxng <yux@nvidia.com>
2020-11-09 16:20:33 -0800

    add flags to roi align setup setting

--- ngc/run_test_job.sh
+++ ngc/run_test_job.sh
@@ -6,15 +6,12 @@ set -e
 set -v
 
 ngc batch run \
-    --instance "dgx1v.16g.2.norm" \
+    --instance "dgx1v.16g.1.norm" \
     --name "test_job" \
     --image "nvcr.io/nvidian/robotics/posecnn-pytorch:latest" \
     --result /result \
-    --datasetid "58777:/deepim/data/models" \
-    --datasetid "58774:/deepim/data/backgrounds" \
-    --datasetid "8187:/deepim/data/coco" \
-    --datasetid "11888:/deepim/data/YCB_Video/YCB_Video_Dataset" \
-    --datasetid "68150:/deepim/data/DEX_YCB/data" \
-    --datasetid "68317:/deepim/data/SUNRGBD" \
-    --workspace deepim:/deepim \
+    --datasetid "58777:/poserbpf/cad_models/ycb_models" \
+    --datasetid "8187:/poserbpf/data/coco" \
+    --datasetid "68150:/poserbpf/data/DEX_YCB/data" \
+    --workspace poserbpf:/poserbpf \
     --commandline "sleep 168h"
--- utils/RoIAlign/setup.py
+++ utils/RoIAlign/setup.py
@@ -37,6 +37,14 @@ def get_extensions():
             "-D__CUDA_NO_HALF_OPERATORS__",
             "-D__CUDA_NO_HALF_CONVERSIONS__",
             "-D__CUDA_NO_HALF2_OPERATORS__",
+            "-arch=sm_50",
+            "-gencode=arch=compute_50,code=sm_50",
+            "-gencode=arch=compute_52,code=sm_52", 
+            "-gencode=arch=compute_60,code=sm_60", 
+            "-gencode=arch=compute_61,code=sm_61", 
+            "-gencode=arch=compute_70,code=sm_70", 
+            "-gencode=arch=compute_75,code=sm_75",
+            "-gencode=arch=compute_75,code=compute_75",
         ]
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
